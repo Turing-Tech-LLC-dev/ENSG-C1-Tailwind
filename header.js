4e22bf813fbe378ef375e337576835502f1b06d9
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-05-16 10:02:22 +0100

    First commit

---
+++ header.js
@@ -0,0 +1,9 @@
+const navbarWrapper = document.getElementById("navbar");
+
+        navbarWrapper.innerHTML = `
+            <span class="bg-blue-600 text-white inline-block w-8 h-8 absolute right-0 bottom-4"></span>
+
+            <span class="bg-teal-600 text-white inline-block w-8 h-8"></span>
+
+            <a href="#features" class="underline">Features</a>
+        `;
